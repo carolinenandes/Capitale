fefa166218254021b9faad24fb1e86d01fc88093
rsjulio <juliorissi5@gmail.com>
2023-08-05 20:29:11 -0300

    initial branch commit

--- app/src/main/java/com/example/tcc20/MainActivity.java
+++ app/src/main/java/com/example/tcc20/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.tcc20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button2 = (Button) findViewById(R.id.button2);
     }
 }
