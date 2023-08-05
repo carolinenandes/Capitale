d0e9b75597c15f8c0589faba2e19ec4cc40f0395
rsjulio <juliorissi5@gmail.com>
2023-08-05 11:51:18 -0300

    initial commit

---
+++ app/src/main/java/com/example/tcc_backend/Prod_Backend.java
@@ -0,0 +1,14 @@
+package com.example.tcc_backend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Prod_Backend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prod_backend);
+    }
+}
