79a6c3a3c01bfe213513beebeedb9aca47a39d19
Caroline Fernandes <carolnandes11@gmail.com>
2023-10-17 00:26:21 -0300

    Design Home Adicionado

---
+++ app/src/main/java/com/example/home_activity.java
@@ -0,0 +1,114 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home
+	 *	@date 		Monday 16th of October 2023 07:22:37 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class home_activity extends Activity {
+
+	
+	private View _bg__home_ek2;
+	private View _bg__functions_ek1;
+	private TextView outrasfuncoes;
+	private View _bg__menubar_ek1;
+	private ImageView menubar_back;
+	private View _bg__menu_icons_ek1;
+	private View _bg__menu_home_ek1;
+	private TextView title_home;
+	private ImageView vector_home_azul;
+	private View _bg__menu_metas_ek1;
+	private TextView title_metas;
+	private ImageView vector_metas;
+	private View _bg__menu_financas_ek1;
+	private TextView title_financas;
+	private ImageView vector_financas;
+	private View _bg__menu_clientes_ek1;
+	private TextView title_clientes;
+	private ImageView vector_clientes;
+	private View _bg__menu_produtos_ek1;
+	private TextView title_produtos;
+	private ImageView vector_produtos;
+	private View _bg__header_ek1;
+	private View header_back;
+	private TextView empresa;
+	private TextView nome;
+	private TextView valor_saldo;
+	private TextView title_saldo_atual;
+	private View profilepic;
+	private View _bg__mainfunctions_ek1;
+	private TextView title_mainfunctions;
+	private View _bg__btn_noticias_ek1;
+	private TextView noticias;
+	private ImageView vector_noticias;
+	private ImageView centerline;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		
+		_bg__home_ek2 = (View) findViewById(R.id._bg__home_ek2);
+		_bg__functions_ek1 = (View) findViewById(R.id._bg__functions_ek1);
+		outrasfuncoes = (TextView) findViewById(R.id.outrasfuncoes);
+		_bg__menubar_ek1 = (View) findViewById(R.id._bg__menubar_ek1);
+		menubar_back = (ImageView) findViewById(R.id.menubar_back);
+		_bg__menu_icons_ek1 = (View) findViewById(R.id._bg__menu_icons_ek1);
+		_bg__menu_home_ek1 = (View) findViewById(R.id._bg__menu_home_ek1);
+		title_home = (TextView) findViewById(R.id.title_home);
+		vector_home_azul = (ImageView) findViewById(R.id.vector_home_azul);
+		_bg__menu_metas_ek1 = (View) findViewById(R.id._bg__menu_metas_ek1);
+		title_metas = (TextView) findViewById(R.id.title_metas);
+		vector_metas = (ImageView) findViewById(R.id.vector_metas);
+		_bg__menu_financas_ek1 = (View) findViewById(R.id._bg__menu_financas_ek1);
+		title_financas = (TextView) findViewById(R.id.title_financas);
+		vector_financas = (ImageView) findViewById(R.id.vector_financas);
+		_bg__menu_clientes_ek1 = (View) findViewById(R.id._bg__menu_clientes_ek1);
+		title_clientes = (TextView) findViewById(R.id.title_clientes);
+		vector_clientes = (ImageView) findViewById(R.id.vector_clientes);
+		_bg__menu_produtos_ek1 = (View) findViewById(R.id._bg__menu_produtos_ek1);
+		title_produtos = (TextView) findViewById(R.id.title_produtos);
+		vector_produtos = (ImageView) findViewById(R.id.vector_produtos);
+		_bg__header_ek1 = (View) findViewById(R.id._bg__header_ek1);
+		header_back = (View) findViewById(R.id.header_back);
+		empresa = (TextView) findViewById(R.id.empresa);
+		nome = (TextView) findViewById(R.id.nome);
+		valor_saldo = (TextView) findViewById(R.id.valor_saldo);
+		title_saldo_atual = (TextView) findViewById(R.id.title_saldo_atual);
+		profilepic = (View) findViewById(R.id.profilepic);
+		_bg__mainfunctions_ek1 = (View) findViewById(R.id._bg__mainfunctions_ek1);
+		title_mainfunctions = (TextView) findViewById(R.id.title_mainfunctions);
+		_bg__btn_noticias_ek1 = (View) findViewById(R.id._bg__btn_noticias_ek1);
+		noticias = (TextView) findViewById(R.id.noticias);
+		vector_noticias = (ImageView) findViewById(R.id.vector_noticias);
+		centerline = (ImageView) findViewById(R.id.centerline);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
