e24624f7fdfe04027523dadb4c2a0af1cc5cb318
alunoinfo <kwillyan200@gmail.com>
2023-10-26 15:23:22 -0300

    Graficos

--- app/src/main/java/com/example/tcc20/SlideScreen.java
+++ app/src/main/java/com/example/tcc20/SlideScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.sunayanpradhan.androidcharts.R;
+
 public class SlideScreen extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/tcc20/financas.java
+++ app/src/main/java/com/example/tcc20/financas.java
@@ -2,13 +2,45 @@ package com.example.tcc20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.sunayanpradhan.androidcharts.R;
 
 public class financas extends AppCompatActivity {
+
+    Button go_bar_chart, go_pie_chart;
+    Gasto_Lucros gasto_lucros;
+    BancoDeDados db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financas);
+
+
+        go_bar_chart = findViewById(R.id.go_bar_chart_button);
+        go_pie_chart = findViewById(R.id.pie_chart_button);
+
+
+        go_pie_chart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(financas.this,PieChartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        go_bar_chart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(financas.this,BarChartActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
