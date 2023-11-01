0aae415e6385ccbb17d8be8915b0987305a43420
kaio_ <kwillyan200@gmail.com>
2023-11-01 01:41:46 -0300

    Clientesss

---
+++ app/src/main/java/com/example/tcc20/ClientesTela.java
@@ -0,0 +1,16 @@
+package com.example.tcc20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sunayanpradhan.androidcharts.R;
+
+public class ClientesTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clientes_tela);
+    }
+}
