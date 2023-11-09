df6d982959f5e59c427449a23bc437f4dead500e
kaio_ <kwillyan200@gmail.com>
2023-11-09 02:23:29 -0300

    meio

--- app/src/main/java/com/example/tcc20/SlideScreen.java
+++ app/src/main/java/com/example/tcc20/SlideScreen.java
@@ -1,14 +1,29 @@
 package com.example.tcc20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SlideScreen extends AppCompatActivity {
+
+    ViewPager mSlideViewPager;
+    LinearLayout mDotLayout;
+    Button btnprox,btnvoltar,btnpular;
+
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide_screen);
+
+        btnprox = findViewById(R.id.btnproximo);
+        btnvoltar = findViewById(R.id.btnvoltar);
+        btnpular = findViewById(R.id.btnpular);
     }
 }
